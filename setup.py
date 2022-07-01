2b0abe23797f3e4f110c4549315608f30153bee0
Diogo Garcia Besson Fernandes <67070481+diogocamarasbc@users.noreply.github.com>
2022-07-01 14:16:30 -0300

    0.1a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='report-classic-project',
-    version='0.2',
+    version='0.1',
     license='MIT',
     author="Câmara Municipal de São Bernardo do Campo, SP, Brazil",
     author_email='informatica@camarasbc.sp.gov.br',
